8d2627a92ab6f7f1c2bfc69be5c2728edd23bb45
LIt4g0 <lit4g0@gmail.com>
2024-08-27 22:07:49 +0200

    Score system v1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 
 public class GameManager : MonoBehaviour
@@ -12,7 +16,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] Canvas menu;
     bool inMenu = true;
     Snake snake;
-    [SerializeField] float totalScore;
+    [SerializeField] int totalScore;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] List<int> scores = new List<int>();
 
     void Awake()
     {
@@ -23,7 +29,7 @@ public class GameManager : MonoBehaviour
         }
         else if (manager != this)
         {
-            Debug.Log("DESTROYING EXTRA MANAGER: " + gameObject.name);
+            //Debug.Log("DESTROYING EXTRA MANAGER: " + gameObject.name);
             Destroy(gameObject);
         }
     }
@@ -38,6 +44,8 @@ public class GameManager : MonoBehaviour
          {
             ShowMenu(true);
          }
+
+         AddScore(PlayerPrefs.GetInt("SavedHighScore"));
     }
 
     void ShowMenu(bool showIn)
@@ -55,10 +63,57 @@ public class GameManager : MonoBehaviour
 
     public void AddScore(int scoreIn)
     {
-        Debug.Log("Added, total is: "+ totalScore);
+        Debug.Log("Added score "+ scoreIn);
         totalScore += scoreIn;
+        scores.Add(scoreIn);
+        SortAndRefreshScores();
         SceneManager.LoadScene("Menu");
         ShowMenu(true);
+    }
+
+    private void SortAndRefreshScores()
+    {
+        string scoreString = "";
+        int i = 0;
+        scores.Sort();
+        scores.Reverse();
+        if (scores.Count >= 10)
+        {
+            scores.RemoveRange(10,scores.Count-10);
+        }
+        //scores.OrderByDescending(10.0f<=1.0f);
+        foreach (var score in scores)
+        {
+            scoreString += i+1 + ". " + scores[i] + "\n";
+            i ++;
+            //if (i >= 10) break;
+        }
+        if (PlayerPrefs.HasKey("SavedHighScore"))
+        {
+            if (PlayerPrefs.GetInt("SavedHighScore") > scores[0])
+            {
+                
+            }
+            else
+            {
+                PlayerPrefs.SetInt("SavedHighScore", scores[0]);
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("SavedHighScore", scores[0]);
+        }
+        Debug.Log(PlayerPrefs.GetInt("SavedHighScore"));
+        scoreText.text = scoreString;
+        // "2. " + scores[2] + "/n" +
+        // "3. " + scores[3] + "/n" +
+        // "4. " + scores[4] + "/n" +
+        // "5. " + scores[5] + "/n" +
+        // "6. " + scores[6] + "/n" +
+        // "7. " + scores[7] + "/n" +
+        // "8. " + scores[8] + "/n" +
+        // "9. " + scores[9] + "/n" +
+        // "10. " + scores[10] + "/n";
     }
 
     public void SetWalls()
